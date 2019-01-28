21d59ec563b3ebabbdc2691b5f9b2e60132b094c
ourhiddenhistory <info@ourhiddenhistory.org>
2019-01-28 16:50:35 -0500

    start work on conversion script

---
+++ toLunr.js
@@ -0,0 +1,33 @@
+#!/bin/node
+
+const SOURCESFILE = './ABC/books'
+const SUBJECTSFILE = './ABC/SUBJECTS'
+const ENTRIESFILE = './ENTRIES-ALL'
+
+const SOURCES = {}
+const SUBJECTS = {}
+const ENTRIES = []
+
+function readLines(input, func) {
+  var remaining = '';
+
+  input.on('data', function(data) {
+    remaining += data;
+    var index = remaining.indexOf('\n');
+    var last  = 0;
+    while (index > -1) {
+      var line = remaining.substring(last, index);
+      last = index + 1;
+      func(line);
+      index = remaining.indexOf('\n', last);
+    }
+
+    remaining = remaining.substring(last);
+  });
+
+  input.on('end', function() {
+    if (remaining.length > 0) {
+      func(remaining);
+    }
+  });
+}
